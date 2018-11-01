6efd6fc18893c4deb81e6676b4070538975e9ca6
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2018-11-01 10:14:53 +0300

    add product component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,83 @@ import React, { Component } from 'react';
 import Product from './components/product'
 
 class App extends Component {
+  state = {
+    products: [
+      { 
+        id: 'foie-gras',
+        isSelected: false,
+        taste: 'с фуа-гра', 
+        weight: '0,5',
+        portions: 10,
+        mouse: 1,
+        longDescription: 'Печень утки разварная с артишоками.',
+      },      
+      { 
+        id: 'fish',
+        isSelected: false,
+        taste: 'с рыбой', 
+        weight: '2',
+        portions: 40,
+        mouse: 2,
+        longDescription: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+      },
+      { 
+        id: 'hen',
+        isSelected: false,
+        taste: 'с курой', 
+        weight: '5',
+        portions: 100,
+        mouse: 5,
+        longDescription: 'Филе из цыплят с трюфелями в бульоне.',
+        additional: 'заказчик доволен',
+      },
+    ],
+  };
+
+  handleSelect = (event, id) => {
+    const productIndex = this.state.products.findIndex(p => {
+      return p.id == id;
+    });
+
+    const product = {
+      ...this.state.products[productIndex]
+    };
+
+    if (product.isSelected ) {
+      product.isSelected = false;
+    } else {
+      product.isSelected = true;
+    }
+    
+    const products = [...this.state.products];
+    products[productIndex] = product;
+    this.setState({products: products});
+  }
+
   render() {
+    let products = null;
+
+    products =  (
+      <div className='products'>
+        {this.state.products.map((product, index) => {
+          return <Product
+            taste={product.taste}
+            weight={product.weight}
+            portions={product.portions}
+            mouse={product.mouse}
+            isSelected={product.isSelected}
+            additional={product.additional}
+            longDescription={product.longDescription}
+            click={(event) => this.handleSelect(event, product.id)}
+            key={product.id} />
+        })}
+      </div>
+    )
+
     return (
-      <div className="products-wrapper">
+      <div className='wrapper'>
         <h1>Ты сегодня покормил кота?</h1>
-          <div className="products">
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-          </div>
+        {products}
       </div>
     );
   }
--- src/components/product.js
+++ src/components/product.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 
-const Product = props => (
-  <div className="product">
-    <div className="product-content">
-      <div className="product__title">Сказочное заморское</div>
-      <div className="product__name">Нямушка</div>
-      <div className="product__taste">с фуа-гра</div>
-      <div className="product__weight">
-        <div className="product__value">0,5</div>
-        <div className="product__unit">кг</div>
-      </div>
+const Product = props => {
+  return (
+    <div className='products__item'>
+      <div className='product-wrapper'>
+        <div className='product' onClick={props.click}>
+          <div className='product__title'>Сказочное заморское яство</div>
+          <div className='product__name'>Нямушка</div>
+          <div className='product__taste'>{props.taste}</div>
+          <div className='product__weight'>
+            <div className='product__value'>{props.weight}</div>
+            <div className='product__unit'>кг</div>
+          </div>
 
-      <div className="product__description">
-        <p><strong>40</strong> порций</p>
-        <p><strong>2</strong> мыши в подарок</p>
-        <p>заказчик доволен</p>
+          <div className='product__description'>
+            <p><strong>{props.portions}</strong> порций</p>
+            <p><strong>{props.mouse}</strong> мыши в подарок</p>
+            {props.additional && <p>{props.additional}</p>}
+          </div>
+        </div>
       </div>
+      
+      {props.isSelected ?
+        <div className='products-footer'>
+          {props.longDescription}
+        </div>
+        :
+        <div className='products-footer'>
+          Чего сидишь? Порадуй котэ, <a href="">купи.</a>
+        </div>
+      }
     </div>
-  </div>
-);
+  ) 
+};
 
 export default Product;
